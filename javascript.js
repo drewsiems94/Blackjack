61d70ff749cf30d0ba840176a890bf90bb38dacd
Andrew Siems <siemsdrew@gmail.com>
2022-08-02 10:42:29 -0700

    Add event listeners.

--- javascript.js
+++ javascript.js
@@ -1,60 +1,73 @@
+// 1=diamond, 2=heart, 3=spade, 4=clover
+const pot = document.getElementById('pot');
+const hit = document.getElementById('hit');
+const stay = document.getElementById('stay');
+
 let playerCards = [];
 let computerCards = [];
-let pot = 0;
+let potTotal = 0;
 let playerTotal = 0;
 let computerTotal = 0;
+let computerSum = 0;
 
-function placeBet() {
-    pot = prompt("How much would you like to bet? $");
-    pot = Number(pot) * 2; 
-}
-
-function dealCards() {
-
-    for (i = 0; i < 4; i++) {
-        if (i == 0) {
-            let number = Math.floor((Math.random() * 10) + 1);
-            let suite = Math.floor((Math.random() * 4) + 1);
-            playerCards[0] = {value: number, suite: suite};
-        } else if (i == 1) {
-            let number = Math.floor((Math.random() * 10) + 1);
-            let suite = Math.floor((Math.random() * 4) + 1);
-            computerCards[0] = {value: number, suite: suite};
-        } else if (i == 2) {
-            let number = Math.floor((Math.random() * 10) + 1);
-            let suite = Math.floor((Math.random() * 4) + 1);
-            playerCards[1] = {value: number, suite: suite};
-        } else {
-            let number = Math.floor((Math.random() * 10) + 1);
-            let suite = Math.floor((Math.random() * 4) + 1);
-            computerCards[1] = {value: number, suite: suite};
-        }
+pot.addEventListener('keypress', function (e) {
+    if (e.code === "Enter") {
+        e.preventDefault();
+        e.stopPropagation();
+        potTotal = Number(e.target.value) * 2;
+        pot.disabled = true;
+        dealCards();
     }
+})
 
-    let hit = prompt("Do you want to hit? Y/N: ");
-    let j = 2;
-    while (hit == "Y") {
-        let number = Math.floor((Math.random() * 10) + 1);
-        let suite = Math.floor((Math.random() * 4) + 1);
+let j = 2;
+hit.addEventListener('click', function hitPlayer () {
+    let number = Math.floor((Math.random() * 10) + 1);
+    let suite = Math.floor((Math.random() * 4) + 1);
+    if (number == 1) {
+        playerCards[j] = {value: number, suite: suite, face: "ace"};
+    } else if (number == 10) {
+        let face = Math.floor((Math.random() * 4) + 1);
+        playerCards[j] = {value: number, suite: suite, face: face};
+    } else {
         playerCards[j] = {value: number, suite: suite};
-        ++j;
-        hit = prompt("Do you want to hit again? Y/N: ");
     }
+    const newDiv = document.createElement("div");
+    newDiv.className = "displayCards";
+    newDiv.setAttribute('id', `pCard${j + 1}`);
+    bottomCards.appendChild(newDiv);
+    ++j;
+});
 
-    let computerSum = computerCards[0].value + computerCards[1].value;
-    let k = 2;
+let k = 2;
+stay.addEventListener('click', function computerPlay () {
+    computerSum = computerCards[0].value + computerCards[1].value;
     while (computerSum < 17) {
         if (computerCards[0].value == 1 && computerCards[1].value == 1) {
             let number = Math.floor((Math.random() * 10) + 1);
             let suite = Math.floor((Math.random() * 4) + 1);
-            computerCards[k] = {value: number, suite: suite};
+            if (number == 1) {
+                computerCards[k] = {value: number, suite: suite, face: "ace"};
+            } else if (number == 10) {
+                let face = Math.floor((Math.random() * 4) + 1);
+                computerCards[k] = {value: number, suite: suite, face: face};
+            } else {
+                computerCards[k] = {value: number, suite: suite};
+            }
             computerSum = computerSum + computerCards[k].value;
             ++k;
         } else if (computerCards[0].value == 1) {
             if (computerCards[1].value + 1 < 17 && computerCards[1].value + 11 < 17) {
                 let number = Math.floor((Math.random() * 10) + 1);
                 let suite = Math.floor((Math.random() * 4) + 1);
-                computerCards[k] = {value: number, suite: suite};
+                if (number == 1) {
+                    computerCards[k] = {value: number, suite: suite, face: "ace"};
+                } else if (number == 10) {
+                    let face = Math.floor((Math.random() * 4) + 1);
+                    computerCards[k] = {value: number, suite: suite, face: face};
+                } else {
+                    computerCards[k] = {value: number, suite: suite};
+                }
                 computerSum = computerSum + computerCards[k].value;
                 ++k;
             } else {
@@ -64,7 +77,14 @@ function dealCards() {
             if (computerCards[0].value + 1 < 17 && computerCards[0].value + 11 < 17) {
                 let number = Math.floor((Math.random() * 10) + 1);
                 let suite = Math.floor((Math.random() * 4) + 1);
-                computerCards[k] = {value: number, suite: suite};
+                if (number == 1) {
+                    computerCards[k] = {value: number, suite: suite, face: "ace"};
+                } else if (number == 10) {
+                    let face = Math.floor((Math.random() * 4) + 1);
+                    computerCards[k] = {value: number, suite: suite, face: face};
+                } else {
+                    computerCards[k] = {value: number, suite: suite};
+                }
                 computerSum = computerSum + computerCards[k].value;
                 ++k;
             } else {
@@ -73,12 +93,69 @@ function dealCards() {
         } else {
             let number = Math.floor((Math.random() * 10) + 1);
             let suite = Math.floor((Math.random() * 4) + 1);
-            computerCards[k] = {value: number, suite: suite};
+            if (number == 1) {
+                computerCards[k] = {value: number, suite: suite, face: "ace"};
+            } else if (number == 10) {
+                let face = Math.floor((Math.random() * 4) + 1);
+                computerCards[k] = {value: number, suite: suite, face: face};
+            } else {
+                computerCards[k] = {value: number, suite: suite};
+            }
             computerSum = computerSum + computerCards[k].value;
             ++k;
         }
     }
+    console.log(checkWinner(playerCards, computerCards));
+})
 
+function dealCards() {
+    for (i = 0; i < 4; i++) {
+        if (i == 0) {
+            let number = Math.floor((Math.random() * 10) + 1);
+            let suite = Math.floor((Math.random() * 4) + 1);
+            if (number == 1) {
+                playerCards[0] = {value: number, suite: suite, face: "ace"};
+            } else if (number == 10) {
+                let face = Math.floor((Math.random() * 4) + 1);
+                playerCards[0] = {value: number, suite: suite, face: face};
+            } else {
+                playerCards[0] = {value: number, suite: suite};
+            }
+        } else if (i == 1) {
+            let number = Math.floor((Math.random() * 10) + 1);
+            let suite = Math.floor((Math.random() * 4) + 1);
+            if (number == 1) {
+                computerCards[0] = {value: number, suite: suite, face: "ace"};
+            } else if (number == 10) {
+                let face = Math.floor((Math.random() * 4) + 1);
+                computerCards[0] = {value: number, suite: suite, face: face};
+            } else {
+                computerCards[0] = {value: number, suite: suite};
+            }
+        } else if (i == 2) {
+            let number = Math.floor((Math.random() * 10) + 1);
+            let suite = Math.floor((Math.random() * 4) + 1);
+            if (number == 1) {
+                playerCards[1] = {value: number, suite: suite, face: "ace"};
+            } else if (number == 10) {
+                let face = Math.floor((Math.random() * 4) + 1);
+                playerCards[1] = {value: number, suite: suite, face: face};
+            } else {
+                playerCards[1] = {value: number, suite: suite};
+            }
+        } else {
+            let number = Math.floor((Math.random() * 10) + 1);
+            let suite = Math.floor((Math.random() * 4) + 1);
+            if (number == 1) {
+                computerCards[1] = {value: number, suite: suite, face: "ace"};
+            } else if (number == 10) {
+                let face = Math.floor((Math.random() * 4) + 1);
+                computerCards[1] = {value: number, suite: suite, face: face};
+            } else {
+                computerCards[1] = {value: number, suite: suite};
+            }
+        }
+    }
 }
 
 function checkWinner(player, computer) {
@@ -92,7 +169,7 @@ function checkWinner(player, computer) {
             playerTotal = playerTotal + playerCards[n].value;
         }
         let highTotal = (playerTotal - 1) + 11;
-        if (hightTotal <= 21) {
+        if (highTotal <= 21) {
             playerTotal = highTotal
         } 
     }
@@ -109,7 +186,6 @@ function checkWinner(player, computer) {
             computerTotal = highTotal
         } 
     }
-    
     if (computerTotal = playerTotal) {
         return "It is a tie.";
     } else if (computerTotal > 21 && playerTotal > 21) {
@@ -124,6 +200,3 @@ function checkWinner(player, computer) {
         return "You lose.";
     }
 }
-
-dealCards();
-checkWinner(playerCards, computerCards);
